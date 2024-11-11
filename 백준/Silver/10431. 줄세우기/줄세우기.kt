023a0bae4d4d39922f1a3e1aa50a5aef4f0429de
SangHyun Ko <87695921+kkosang@users.noreply.github.com>
2024-11-12 08:29:34 +0900

    [Silver V] Title: 줄세우기, Time: 224 ms, Memory: 26044 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.kt
+++ 백준/Silver/10431. 줄세우기/줄세우기.kt
@@ -7,24 +7,34 @@ fun main() {
         val input = readln().split(" ").map { it.toInt() }
         val case = input[0]
         val students = input.subList(1, input.size)
-        var count = 0
 
         val line = mutableListOf<Int>()
-        students.forEach { student ->
-            // 삽입 위치 찾기
-            var position = line.size
-            for (i in line.indices) {
-                if (student < line[i]) {
-                    position = i
-                    break
-                }
-            }
-            count = count + line.size - position
-            line.add(position, student)
-        }
-
+        val count = sortedCount(line, students)
+        
         sb.appendLine("$case $count")
     }
 
     println(sb)
+}
+
+private fun sortedCount(line: MutableList<Int>, students: List<Int>): Int {
+    var count = 0
+
+    students.forEach { student ->
+        val position = insertPosition(line, student)
+        count += line.size - position
+        line.add(position, student)
+    }
+
+    return count
+}
+
+private fun insertPosition(line: List<Int>, student: Int): Int {
+    for (i in line.indices) {
+        if (student < line[i]) {
+            return i
+        }
+    }
+
+    return line.size // 맨 뒤에 추가되는 경우
 }
